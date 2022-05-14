cf14f563e72d37d4cf9000a526b7bf902b6f97ee
Konrad Ambroży <konrad.ambrozy9@gmail.com>
2022-05-14 14:29:08 +0200

    types

--- src/types.ts
+++ src/types.ts
@@ -15,3 +15,28 @@ export type PlatiniumTrophy = {
     timePlayed: string;
     completionPercentage: number;
 };
+
+
+export type SteamGame = {
+    appid: string;
+    name: string;
+    img_icon_url:string;
+    img_logo_url:string;
+    playtime_forever: number;
+    playtime_windows_forever: number;
+    playtime_mac_forever: number;
+    playtime_linux_forever: number;
+}
+
+export type SteamGameData = {
+    steamID: string;
+    gameName: string;
+    achievements: SteamAchievement[];
+    success: boolean;
+}
+
+export type SteamAchievement = {
+    apiname: string;
+    achieved: number;
+    unlocktime: number;
+}
