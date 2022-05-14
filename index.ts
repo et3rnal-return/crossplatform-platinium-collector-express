5bee58a6c704c9ae1eefa3cc80dacbf6b0e11f40
Konrad Ambroży <konrad.ambrozy9@gmail.com>
2022-05-14 14:30:13 +0200

    wip

--- index.ts
+++ index.ts
@@ -2,15 +2,15 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 const psn = require("psn-api");
 const trophies = require('./src/trophies')
-
+const cors = require("cors");
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+app.use(cors());
 app.use(express.json());
 app.use('/trophies', trophies);
 console.log(trophies)
-// app.use(cors());
 app.get('/asd', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
